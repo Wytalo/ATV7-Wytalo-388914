99ca15eeb3fc2602e4e93e193df44041ab7f6f9b
Wytalo <31418745+Wytalo@users.noreply.github.com>
2021-04-02 17:14:15 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import 'react-native-gesture-handler';
+import React from 'react';
+import Routes from './components/Routes';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
+};
+
+export default App;
